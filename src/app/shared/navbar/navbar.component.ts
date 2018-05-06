e6c2ee468807810b6282bfe16ed8542c9847a1c8
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-06 18:15:16 +0100

    Added basic navbar with some elements not wired up yet

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-
+public isCollapsed = true;
   constructor() { }
 
   ngOnInit() {
