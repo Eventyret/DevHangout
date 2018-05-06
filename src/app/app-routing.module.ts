27fac5a913cb3bddb9a24ffa8c8cee47a4b21bb6
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-06 14:50:46 +0100

    Started scaffolding dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { ProfileComponent } from './dashboard/profile/profile.component';
+import { EditComponent } from './dashboard/profile/edit/edit.component';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [DashboardComponent, ProfileComponent, EditComponent]
 })
 export class AppRoutingModule { }
