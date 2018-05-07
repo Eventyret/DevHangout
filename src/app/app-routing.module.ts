b4a156e494ccdc4cac4c5032a9d835b3be479386
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-07 01:06:54 +0100

    Recreated login / Register into one page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginComponent } from "./shared/register/login/login.component";
+import { RegisterComponent } from "./shared/register/register.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
@@ -5,9 +7,8 @@ import { ProfileComponent } from "./dashboard/profile/profile.component";
 import { EditComponent } from "./dashboard/profile/edit/edit.component";
 import { AddComponent } from "./dashboard/profile/add/add.component";
 import { FeedComponent } from "./feed/feed/feed.component";
+
 import { PostsComponent } from "./feed/posts/posts.component";
-import { RegisterComponent } from "./signup-login/register/register.component";
-import { LoginComponent } from "./signup-login/login/login.component";
 import { LandingComponent } from "./landing/landing.component";
 import { ProfilesComponent } from "./profiles/profiles.component";
 import { PageNotFoundComponent } from "./shared/page-not-found/page-not-found.component";
