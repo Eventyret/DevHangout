d1195ed80a49cf7d4612e85920314696f59ddc98
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-07 13:51:33 +0100

    Layout with tabs completed, missing info in each tab

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from "@angular/core";
+import {NgbTabsetConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-profile",
   templateUrl: "./profile.component.html",
-  styleUrls: ["./profile.component.scss"]
+  styleUrls: ["./profile.component.scss"],
+  providers: [NgbTabsetConfig]
+
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  constructor(config: NgbTabsetConfig) {
+	  config.justify = "justified";
+	  config.type = "pills";
+   }
 
   ngOnInit() {
   }
