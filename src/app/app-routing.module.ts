0f1bbdb94ec4efd658ed8e4bd0a42f0943550d5e
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-07 12:13:39 +0100

    added routing to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ const routes: Routes = [
 	{ path: "", component: LandingComponent, pathMatch: "full" },
 	{ path: "login", component: LoginComponent, pathMatch: "full"},
 	{ path: "signup", component: SignupComponent, pathMatch: "full"},
+	{ path: "profile", component: ProfileComponent, pathMatch: "full"},
 	{ path: "auth", component: RegisterComponent},
 	{ path: "dashboard", component: DashboardComponent },
 	{ path: "**", component: PageNotFoundComponent } // Page not found
