fe23aa201c7a611f37e0f4105becd2cb9904969e
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-08 23:30:19 +0100

    Prepered Click button for opening Modal

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { AddEducationComponent } from "./profile/education/add/add.component";
 
 @Component({
   selector: "app-dashboard",
@@ -7,9 +9,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
-
+  open() {
+	const modalRef = this.modalService.open(AddEducationComponent);
+}
 }
