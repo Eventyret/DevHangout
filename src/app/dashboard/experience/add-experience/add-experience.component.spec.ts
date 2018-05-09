4e607df9e7e6894131cf03d3a186c86a5524549f
Simen Daehlin <sdaehlin@intergage.uk>
2018-05-09 08:58:28 +0100

    Updated and added correct components

---
+++ src/app/dashboard/experience/add-experience/add-experience.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddExperienceComponent } from './add-experience.component';
+
+describe('AddExperienceComponent', () => {
+  let component: AddExperienceComponent;
+  let fixture: ComponentFixture<AddExperienceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddExperienceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddExperienceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
