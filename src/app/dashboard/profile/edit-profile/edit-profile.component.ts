e8235f4ef029a4c67cb39560374208f815a217aa
Simen Daehlin <sdaehlin@intergage.uk>
2018-05-10 13:29:49 +0100

    Markup added for edit profile

--- src/app/dashboard/profile/edit-profile/edit-profile.component.ts
+++ src/app/dashboard/profile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import {NgbModal, NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-edit-profile",
@@ -7,7 +8,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class EditProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit() {
   }
