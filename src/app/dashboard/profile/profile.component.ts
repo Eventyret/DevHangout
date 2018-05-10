5cc9e6c3d9aabca07423a47a9c79cc9095e69b0b
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-10 19:12:56 +0100

    added spinner

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from "@angular/core";
-import {NgbTooltipConfig } from "@ng-bootstrap/ng-bootstrap";
+import { NgbTooltipConfig } from "@ng-bootstrap/ng-bootstrap";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
-  selector: "app-profile",
-  templateUrl: "./profile.component.html",
-  styleUrls: ["./profile.component.scss"],
-  providers: [NgbTooltipConfig]
-
+	selector: "app-profile",
+	templateUrl: "./profile.component.html",
+	styleUrls: ["./profile.component.scss"],
+	providers: [NgbTooltipConfig]
 })
 export class ProfileComponent implements OnInit {
-  constructor(config: NgbTooltipConfig, ) {
-	config.placement = "top";
-	config.triggers = "hover";
-   }
-   support = true;
+	constructor(config: NgbTooltipConfig, private spinner: NgxSpinnerService) {
+		config.placement = "top";
+		config.triggers = "hover";
+	}
+	support = true;
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.spinner.show();
+		setTimeout(() => {
+			/** spinner ends after 5 seconds */
+			this.spinner.hide();
+		}, 2000);
+	}
 
-  supporterTest() {
-	  this.support = !this.support;
-  }
-
+	supporterTest() {
+		this.support = !this.support;
+	}
 }
