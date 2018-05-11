502b51dd2e1c79eaa7a58e73152c99ebd12692a0
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-11 19:15:50 +0100

    Added a demo stripe module to test with Credits to ngx-stripe

--- src/app/shared/supporter/supporter-modal/supporter-modal.component.ts
+++ src/app/shared/supporter/supporter-modal/supporter-modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+import { StripeService, Elements, Element as StripeElement, ElementsOptions } from "ngx-stripe";
+
 import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
@@ -7,7 +11,55 @@ import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 	styleUrls: ["./supporter-modal.component.scss"]
 })
 export class SupporterModalComponent implements OnInit {
-	constructor(public activeModal: NgbActiveModal) {}
+	elements: Elements;
+	card: StripeElement;
 
-	ngOnInit() {}
+	// optional parameters
+	elementsOptions: ElementsOptions = {
+		locale: "auto"
+	};
+
+	stripeTest: FormGroup;
+	constructor(public activeModal: NgbActiveModal, private fb: FormBuilder, private stripeService: StripeService) {}
+
+	ngOnInit() {
+		this.stripeTest = this.fb.group({
+			name: ["", [Validators.required]]
+		});
+		this.stripeService.elements(this.elementsOptions).subscribe(elements => {
+			this.elements = elements;
+			// Only mount the element the first time
+			if (!this.card) {
+				this.card = this.elements.create("card", {
+					style: {
+						base: {
+							iconColor: "#666EE8",
+							color: "#31325F",
+							lineHeight: "40px",
+							fontWeight: 300,
+							fontFamily: "Helvetica Neue, Helvetica, sans-serif",
+							fontSize: "18px",
+							"::placeholder": {
+								color: "#CFD7E0"
+							}
+						}
+					}
+				});
+				this.card.mount("#card-element");
+			}
+		});
+	}
+	buy() {
+		const name = this.stripeTest.get("name").value;
+		this.stripeService.createToken(this.card, { name }).subscribe(token => {
+			if (token) {
+				// Use the token to create a charge or a customer
+				// https://stripe.com/docs/charges
+				console.log(token);
+			} else if (Error) {
+				// Error creating the token
+				console.log(token.error.message);
+			}
+		});
+	}
 }
