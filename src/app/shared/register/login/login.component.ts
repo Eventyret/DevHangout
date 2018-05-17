9e51c819b32d5580568f1a748f40eb142230142f
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-17 22:07:04 +0100

    Started adding formfields

--- src/app/shared/register/login/login.component.ts
+++ src/app/shared/register/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+	selector: "app-login",
+	templateUrl: "./login.component.html",
+	styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+	loginForm = new FormGroup({
+		email: new FormControl(null, [Validators.required, Validators.email]),
+		password: new FormControl("", Validators.required)
+	});
 
-  constructor() { }
+	get emailField() {
+		return this.loginForm.get("email");
+	}
+	constructor() {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
