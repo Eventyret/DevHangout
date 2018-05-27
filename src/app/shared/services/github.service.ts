9a875f41a670a8e820631a89a4662cd010fbafb7
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-27 16:08:17 +0100

    created api call to get repos

--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { environment } from "../../../environments/environment";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
+import "rxjs/add/operator/catch";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root"
 })
 export class GithubService {
+	clientID: string = environment.github_client_id;
+	clientSecret: string = environment.github_client_secret;
+	githubUrl = "https://api.github.com/";
 
-  constructor() { }
+	constructor(private http: HttpClient) {}
+
+	gitRepo(username) {
+		return this.http
+			.get(this.githubUrl + username + "/" + "repos")
+			.pipe(map((data: any) => data))
+			.catch((error: any) => {
+				return Observable.throw(error);
+			});
+	}
 }
