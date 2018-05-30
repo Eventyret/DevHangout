56c939d5809df6f756bc1175a5af186d3c5674e5
Simen Daehlin <simen.dehlin@gmail.com>
2018-05-31 00:07:48 +0100

    started working on JWT

--- BackendAPI/BackendAPI/settings.py
+++ BackendAPI/BackendAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import rest_framework
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	"rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+	"DEFAULT_AUTHENTICATION_CLASSES" : ("rest_framework_simplejwt.authentication.JWTAuthentication",)
+}
