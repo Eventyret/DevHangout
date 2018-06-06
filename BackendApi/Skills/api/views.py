eb6351c48f621abbe25198ace6819a4511fe78c4
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-06 21:29:38 +0100

    Added pylint for django and updated models ready to add data

---
+++ BackendApi/Skills/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from ..models import Skills
+from .serializers import SkillsSerializer
+
+# Creating a view for the api endpoint
+
+
+class SkillsView(viewsets.ReadOnlyModelViewSet):
+	""" Settings it to read only as its not needed to be updated or deleted from the frontend """
+	queryset = Skills.objects.all()
+	serializer_class = SkillsSerializer
+
