3728bb1327894895d4658cc9d5e7f3175f8c0afc
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-06 19:53:14 +0100

    Added model

--- Skills/models.py
+++ Skills/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Skills(models.Model):
+	ID = models.AutoField(primary_key=True)
+	Name = models.CharField(max_length=50)
+	DisplayName = models.CharField(max_length=50)
