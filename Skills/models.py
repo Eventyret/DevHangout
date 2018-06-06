a0383806f95a392006b205a3a688f720e011df27
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-06 20:02:04 +0100

    Updated name and settings

--- Skills/models.py
+++ Skills/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class Skills(models.Model):
-	ID = models.AutoField(primary_key=True)
-	Name = models.CharField(max_length=50)
-	DisplayName = models.CharField(max_length=50)
+    ID = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    DisplayName = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Skills"
