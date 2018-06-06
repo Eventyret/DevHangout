8b30834a5bd2a4749664869ad460a8c49e20c732
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-06 21:29:38 +0100

    Added pylint for django and updated models ready to add data

---
+++ Skills/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from ..models import Skills
+
+
+class SkillsSerializer(serializers.ModelSerializer):
+    """ A Serializer that will have read only access but using the modal one """
+    class Meta:
+        model = Skills
+        fields = ("id", "Name", "DisplayName")
