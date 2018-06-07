fc286e80520c28add309a4ebbc916f0821237c2b
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-07 16:37:03 +0100

    Added JWT

--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('Skills.urls')),
-    path("api/auth/", include("rest_framework.urls"))
+    path("api/auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view())
 ]
