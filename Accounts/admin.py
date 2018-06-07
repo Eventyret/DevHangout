2036becf1a46d41a3b0fa498c70ca13a83ae7356
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-07 21:06:18 +0100

    Fixed error with Accounts

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Profiles
+from .models import Profile
 # Register your models here.
 
-admin.site.register(Profiles)
+admin.site.register(Profile)
