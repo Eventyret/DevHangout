093bc29912287139e13d151103725bd39e2a05a0
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-07 21:06:18 +0100

    Fixed error with Accounts

--- BackendApi/Accounts/models.py
+++ BackendApi/Accounts/models.py
@@ -6,26 +6,26 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    avatar = models.ImageField(upload_to="uploads/avatars", max_length=None)
+    firstName = models.CharField(max_length=50, blank=True, null=True)
+    lastName = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(upload_to="uploads/avatars", max_length=None, blank=True, null=True)
     email = models.EmailField(max_length=254)
-    location = models.CharField(max_length=50)
-    website = models.CharField(max_length=50)
-    company = models.CharField(max_length=50)
-    title = models.CharField(max_length=50)
-    backgroundImage = models.ImageField(upload_to="uploads/backgrounds", max_length=None)
-    bio = models.TextField()
-    twitterURL = models.CharField(max_length=100)
-    facebookURL = models.CharField(max_length=100)
-    linkedinURL = models.CharField(max_length=100)
-    instagramURL = models.CharField(max_length=100)
-    youtubeURL = models.CharField(max_length=100)
-    githubUserName = models.CharField(max_length=50)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    website = models.CharField(max_length=50, blank=True, null=True)
+    company = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=50, blank=True, null=True)
+    backgroundImage = models.ImageField(upload_to="uploads/backgrounds", max_length=None, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    twitter = models.CharField(max_length=100, blank=True, null=True)
+    facebook = models.CharField(max_length=100, blank=True, null=True)
+    linkedin = models.CharField(max_length=100, blank=True, null=True)
+    instagram = models.CharField(max_length=100, blank=True, null=True)
+    youtube = models.CharField(max_length=100, blank=True, null=True)
+    github = models.CharField(max_length=50, blank=True, null=True)
     donator = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = "Profile"
 
     def __str__(self):
-        return self.user
+        return self.user.username
