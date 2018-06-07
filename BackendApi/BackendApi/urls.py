ae6d36443c9d21c05cc6a553cb534ed4e0984572
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-07 16:12:21 +0100

    Started on authentication

--- BackendApi/BackendApi/urls.py
+++ BackendApi/BackendApi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('Skills.urls'))
+    path("", include('Skills.urls')),
+    path("api/auth/", include("rest_framework.urls"))
 ]
