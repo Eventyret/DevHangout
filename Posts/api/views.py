234886199e3f5f03c7403b37c000cce199821685
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-09 00:12:28 +0100

    Updated models

--- Posts/api/views.py
+++ Posts/api/views.py
@@ -5,5 +5,5 @@ from .serializers import PostSerializer
 
 class PostsView(viewsets.ModelViewSet):
     """ Settings it to read only as its not needed to be updated or deleted from the frontend """
-    queryset = Skills.objects.all()
-    serializer_class = SkillsSerializer
+    queryset = Posts.objects.all()
+    serializer_class = PostSerializer
