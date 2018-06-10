75ef756ec51e47e510d8ab8f96cef8ee8879ed6e
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-10 10:26:22 +0100

    Added Skill List

--- Skills/models.py
+++ Skills/models.py
@@ -11,4 +11,14 @@ class Skills(models.Model):
 		verbose_name_plural = "Skills"
 
 	def __str__(self):
-		return "Skill: " + self.DisplayName
+		return self.DisplayName
+class SkillList(models.Model):
+	"""List of skills that a developer can pick from"""
+	name = models.CharField(max_length=50)
+	displayName = models.CharField(max_length=50)
+
+	class Meta:
+		verbose_name_plural = "Skill List"
+
+	def __str__(self):
+		return self.DisplayName
