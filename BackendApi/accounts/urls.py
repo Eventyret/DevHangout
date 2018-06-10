16e372e2569a81670eee28999dfb3b83117fd152
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-10 19:33:49 +0100

    Fixed an error added single profile

--- BackendApi/accounts/urls.py
+++ BackendApi/accounts/urls.py
@@ -4,6 +4,7 @@ from .api import views
 
 ROUTER = routers.DefaultRouter()
 ROUTER.register("api/profile", views.ProfileView)
+ROUTER.register("api/profile/(?P<id>[0-9])/$", views.ProfileView)
 
 urlpatterns = [
     path("", include(ROUTER.urls))
