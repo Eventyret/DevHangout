6558171bae9b464b3d53a7f03c571490a081fe0f
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 15:58:00 +0100

    Removed email field as it was not needed

--- BackendApi/accounts/admin.py
+++ BackendApi/accounts/admin.py
@@ -2,17 +2,16 @@ from django.contrib import admin
 from .models import Profile
 # Register your models here.
 
-
 class AccountAdmin(admin.ModelAdmin):
     """
     Custom View of Accounts
     """
 
-    list_display = ("user", "email", "location", "title", "github", "donator", )
+    list_display = ("user", "location", "title", "github", "donator", )
 
     list_filter = ("donator", )
 
-    search_fields = ("email", "firstName", "lastName", "github", )
+    search_fields = ("firstName", "lastName", "github", )
 
     ordering = ["-user__username"]
 
