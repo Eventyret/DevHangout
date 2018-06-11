de255cec4079b848a08a9645f4ca700ac7ceac4b
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 21:21:24 +0100

    removed email as its not in the model anymore

--- BackendApi/accounts/api/serializers.py
+++ BackendApi/accounts/api/serializers.py
@@ -7,7 +7,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     """A Serializer to use CRUD on a user profile"""
     class Meta:
         model = Profile
-        fields = ("user", "email", "firstName", "lastName", "avatar", "location", "website", "company",
+        fields = ("user", "firstName", "lastName", "avatar", "location", "website", "company",
                   "backgroundImage", "bio", "twitter", "facebook", "linkedin", "instagram", "youtube", "github", "donator")
 
 
