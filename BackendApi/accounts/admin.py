7eab949d1e199d67774db3dee5b431e5ad58b142
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 21:21:14 +0100

    adding email

--- BackendApi/accounts/admin.py
+++ BackendApi/accounts/admin.py
@@ -6,14 +6,17 @@ class AccountAdmin(admin.ModelAdmin):
     """
     Custom View of Accounts
     """
-
-    list_display = ("user", "location", "title", "github", "donator", )
+    list_display = ("user", "get_user_email", "location", "title", "github", "donator", )
 
     list_filter = ("donator", )
 
     search_fields = ("firstName", "lastName", "github", )
 
     ordering = ["-user__username"]
+    def get_user_email(self, instance):
+        return instance.user.email
+    get_user_email.short_description = 'User Email'
+
 
 
 admin.site.register(Profile, AccountAdmin)
