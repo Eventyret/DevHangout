dc45d86c136d108467f7a374f7d28126ecf586c9
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 08:22:41 +0100

    added reciver for user registration time

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 	# Create your models here.
 
@@ -23,6 +25,15 @@ class Profile(models.Model):
 	youtube = models.CharField(max_length=100, blank=True, null=True)
 	github = models.CharField(max_length=50, blank=True, null=True)
 	donator = models.BooleanField(default=False)
+
+	@receiver(post_save, sender=User)
+	def create_user_profile(sender, instance, created, **kwargs):
+		if created:
+			Profile.objects.create(user=instance)
+
+	@receiver(post_save, sender=User)
+	def save_user_profile(sender, instance, **kwargs):
+		instance.profile.save()
 
 	class Meta:
 		verbose_name_plural = "Profile"
