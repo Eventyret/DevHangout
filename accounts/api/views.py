8e520ff85d2aad2226370777ced86e2e721de07b
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 21:21:07 +0100

    removed render as its not needed

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
 from ..models import Profile
 from .serializers import ProfileSerializer, UserSerializer
+
 
 # Creating a view for the api endpoint
 
@@ -10,6 +10,7 @@ from .serializers import ProfileSerializer, UserSerializer
 class ProfileView(viewsets.ModelViewSet):
 	queryset = Profile.objects.all()
 	serializer_class = ProfileSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
