4fbb1c649eb29c0eaa4104edcbdab3e06b14b745
Simen Daehlin <simen.dehlin@gmail.com>
2018-06-11 15:58:00 +0100

    Removed email field as it was not needed

--- accounts/models.py
+++ accounts/models.py
@@ -11,7 +11,6 @@ class Profile(models.Model):
 	firstName = models.CharField(max_length=50, blank=True, null=True)
 	lastName = models.CharField(max_length=50, blank=True, null=True)
 	avatar = models.ImageField(upload_to="uploads/avatars", max_length=None, blank=True, null=True)
-	email = models.EmailField(max_length=254)
 	location = models.CharField(max_length=50, blank=True, null=True)
 	website = models.CharField(max_length=50, blank=True, null=True)
 	company = models.CharField(max_length=50, blank=True, null=True)
