cd16ca413443a8969fbd2a61be8043d8c04e8310
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-06 15:55:14 +0100

    Updated permission views

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated", ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny", ),
     "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication", )
 }
 
--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -10,17 +10,26 @@ class ProfileSerializer(serializers.ModelSerializer):
         fields = ("user", "firstName", "lastName", "avatar", "location", "website", "company",
                   "backgroundImage", "bio", "twitter", "facebook", "linkedin", "instagram", "youtube", "github", "donator")
 
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ("id", "username", "email", "password")
-        extra_kwargs = {
-			"password": {
-				"write_only": True,
-				"required": True
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ("id", "username", "email", "password")
+		write_only_fields = ('password',)
+		read_only_fields = ('id',)
+		extra_kwargs = {
+				"password": {
+					"write_only": True,
+					"required": True
 				}}
 
-        def create(self, validate_date):
-            User = User.objects.create_user(**validated_data)
-            return User
+
+	def create(self, validated_data):
+		user = User.objects.create(
+			username=validated_data['username'],
+			email=validated_data['email']
+		)
+
+		user.set_password(validated_data['password'])
+		user.save()
+
+		return user
