92a63f14dbfd5eb56c511555654f89fac5dbec11
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-08 18:46:29 +0100

    Added ability to query data

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbTooltipConfig } from "@ng-bootstrap/ng-bootstrap";
 import { NgxSpinnerService } from "ngx-spinner";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Users } from "../../shared/models/users";
 import { FakeService } from "../../shared/services/api/fake.service";
+import { find as _find } from "lodash";
 
 @Component({
 	selector: "app-profile",
@@ -12,7 +13,13 @@ import { FakeService } from "../../shared/services/api/fake.service";
 	providers: [NgbTooltipConfig]
 })
 export class ProfileComponent implements OnInit {
-	constructor(config: NgbTooltipConfig, private spinner: NgxSpinnerService, private route: ActivatedRoute, private fakeService: FakeService) {
+	constructor(
+		config: NgbTooltipConfig,
+		private spinner: NgxSpinnerService,
+		private route: ActivatedRoute,
+		private fakeService: FakeService,
+		private router: Router
+	) {
 		config.placement = "top";
 		config.triggers = "hover";
 		this.route.params.subscribe(params => {
@@ -20,7 +27,7 @@ export class ProfileComponent implements OnInit {
 			this.getFakeData(id);
 		});
 	}
-	user: Users;
+	user: any;
 	support: boolean;
 
 	ngOnInit() {
@@ -33,8 +40,11 @@ export class ProfileComponent implements OnInit {
 	getFakeData(id) {
 		this.fakeService.getFakeUsers().subscribe(
 			data => {
-				this.user = data[id];
-				this.support = data[id].supporter;
+				const userID = id;
+				this.user = _find(data, function(o) {
+					return o.id == userID;
+				});
+				this.support = this.user.supporter;
 			},
 			error => {
 				console.log(error);
