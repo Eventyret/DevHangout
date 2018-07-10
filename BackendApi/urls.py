3314e53174d759641c9b7f3898052ab15ceffdb5
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-10 17:32:44 +0100

    Started work on navbar image avatar. Needs backend fix for static files

--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path("blog/", include("blog.urls"))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +28,4 @@ urlpatterns = [
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view())
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
