7cf1bc2afb1ee82000bc514038d3e1b878028273
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-11 17:19:55 +0100

    Fixed so media and uploaded files and avatars are displayed

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = "/static/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny", ),
--- BackendApi/urls.py
+++ BackendApi/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path("api/auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("api/token/", TokenObtainPairView.as_view()),
     path("api/token/refresh/", TokenRefreshView.as_view())
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
