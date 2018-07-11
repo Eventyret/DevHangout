a5fbb0621f93fa3cf0773e20d7dae238f7df80b6
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-11 16:51:01 +0100

    Updated to isOwner to check if the user own the profile

--- BackendApi/accounts/api/permissions.py
+++ BackendApi/accounts/api/permissions.py
@@ -1,16 +1,12 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+
+class IsOwner(permissions.BasePermission):
     """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
+    Custom permission to only allow owners of an object to edit it.
     """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated()
 
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        # Instance must have an attribute named `owner`.
         return obj.user == request.user
--- BackendApi/accounts/api/views.py
+++ BackendApi/accounts/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsOwner
 from ..models import Profile, Education, Experience
 from .serializers import ProfileSerializer, UserSerializer, EducationSerializer, ExperienceSerializer, UserRegistrationSerializer
 
@@ -10,17 +10,17 @@ from .serializers import ProfileSerializer, UserSerializer, EducationSerializer,
 
 
 class ProfileView(viewsets.ModelViewSet):
-	permission_classes = (IsOwnerOrReadOnly, )
+	permission_classes = (IsOwner, )
 	queryset = Profile.objects.all()
 	serializer_class = ProfileSerializer
 
 class EducationView(viewsets.ModelViewSet):
-	permission_classes = (IsAuthenticatedOrReadOnly, )
+	permission_classes = (IsOwner, )
 	queryset = Education.objects.all()
 	serializer_class = EducationSerializer
 
 class ExperienceView(viewsets.ModelViewSet):
-	permission_classes = (IsAuthenticatedOrReadOnly, )
+	permission_classes = (IsOwner, )
 	queryset = Experience.objects.all()
 	serializer_class = ExperienceSerializer
 
