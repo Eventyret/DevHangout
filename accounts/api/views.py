5072c4c4a358b940fff572915eeab2cb495d2d89
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-11 14:28:52 +0100

    Updated and made many to one for Exp and Edu.

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django.contrib.auth.models import User
 from ..models import Profile, Education, Experience
-from .serializers import ProfileSerializer, UserSerializer, EducationSerializer, ExperienceSerializer
+from .serializers import ProfileSerializer, UserSerializer, EducationSerializer, ExperienceSerializer, UserRegistrationSerializer
 
 
 # Creating a view for the api endpoint
@@ -14,13 +14,20 @@ class ProfileView(viewsets.ModelViewSet):
 	serializer_class = ProfileSerializer
 
 class EducationView(viewsets.ModelViewSet):
+	permission_classes = (IsAuthenticatedOrReadOnly, )
 	queryset = Education.objects.all()
 	serializer_class = EducationSerializer
 
 class ExperienceView(viewsets.ModelViewSet):
+	permission_classes = (IsAuthenticatedOrReadOnly, )
 	queryset = Experience.objects.all()
 	serializer_class = ExperienceSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+	permission_classes = (IsAuthenticatedOrReadOnly, )
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+class UserRegistrationViewSet(viewsets.ModelViewSet):
+	queryset = User.objects.all()
+	serializer_class =UserRegistrationSerializer
