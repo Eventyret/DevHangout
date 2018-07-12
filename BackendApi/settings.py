299209fe2c18179d5edc195bc6c2f27161c815e4
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-12 21:26:53 +0100

    Corrected path problems.

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny", ),
