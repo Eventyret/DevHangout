edcdd982fffca2f3d6b5222e140d29fec7507971
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-18 22:42:33 +0100

    added timer for refresh token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { interval } from "rxjs";
+import { AuthService } from "./shared/services/auth/auth.service";
 
 @Component({
 	selector: "app-root",
 	templateUrl: "./app.component.html",
 	styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+	constructor(private authService: AuthService) {}
+	ngOnInit() {
+		
+		// Refreshing token every 8 minutes
+		interval(480000).subscribe(x => {
+			this.authService.refreshToken();
+		});
+	}
+}
