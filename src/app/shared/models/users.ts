d1125feeca8e5753e37d6d676c444b85458ac72c
Simen Daehlin <simen.dehlin@gmail.com>
2018-07-18 07:59:16 +0100

    updated user interface

--- src/app/shared/models/users.ts
+++ src/app/shared/models/users.ts
@@ -1,12 +1,5 @@
-export interface User {
-	id: number;
-	username: string;
-	email: string;
-	profile: Profile;
-}
-
 export interface Profile {
-	user: string;
+	user: number;
 	firstName: string;
 	lastName: string;
 	avatar: string;
@@ -18,14 +11,59 @@ export interface Profile {
 	bio: string;
 	twitter: string;
 	facebook: string;
+	linkedin: string;
 	instagram: string;
 	youtube: string;
 	github: string;
 	donator: boolean;
-	skills: Skills;
+	testimonal: string;
 }
-export interface Skills {
+
+export interface Education {
+	user: number;
+	school: string;
+	qualification: string;
+	fieldOfStudy: string;
+	dateFrom: string;
+	dateTo: string;
+	description: string;
+}
+
+export interface Experience {
+	user: number;
+	jobTitle: string;
+	company: string;
+	dateFrom: string;
+	dateTo: string;
+	current: boolean;
+	description: string;
+	location: string;
+}
+
+export interface Skill {
 	id: number;
 	name: string;
 	icon: string;
+}
+
+export interface Repo {
+	name: string;
+	html_url: string;
+	description: string;
+	stargazers_count: number;
+	watchers_count: number;
+	forks_count: number;
+	open_issues_count: number;
+	updated_at: string;
+}
+
+export interface User {
+	id: number;
+	username: string;
+	email: string;
+	profile: Profile;
+	education: Education[];
+	experience: Experience[];
+	skills: Skill[];
+	repo: Repo[];
 }
