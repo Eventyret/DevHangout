04eb4d1c012bd8674fe03272f00861ae033c019a
Simen Daehlin <sdaehlin@intergage.co.uk>
2018-08-03 22:41:06 +0100

    updated and added put / patch function for edu and exp

--- accounts/api/permissions.py
+++ accounts/api/permissions.py
@@ -6,7 +6,7 @@ class IsOwner(permissions.BasePermission):
     Custom permission to only allow owners of an object to edit it.
     """
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated()
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
