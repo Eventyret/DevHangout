f6222de3efd29387c6a98258035af4c6a59a842e
Simen Daehlin <sdaehlin@intergage.co.uk>
2018-08-03 23:40:47 +0100

    removed function not used

--- BackendApi/Skills/api/views.py
+++ BackendApi/Skills/api/views.py
@@ -10,6 +10,3 @@ class SkillsView(viewsets.ModelViewSet):
     """ Settings it to read only as its not needed to be updated or deleted from the frontend """
     queryset = Skill.objects.all()
     serializer_class = SkillsSerializer
-
-	def def get_queryset(self):
-		user_id = self.kwargs["id"]
