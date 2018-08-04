900a702e1612c042d1a2b2385e99d178b2c751d7
Simen Daehlin <sdaehlin@intergage.co.uk>
2018-08-04 13:36:09 +0100

    Removed Posts from system

--- BackendApi/settings.py
+++ BackendApi/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "Skills",
-	"Posts",
     "accounts",
 	"corsheaders"
 ]
--- Posts/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import Posts
-
-class PostSerializer(serializers.ModelSerializer):
-    """ A Serializer that will have read only access but using the modal one """
-    class Meta:
-        model = Posts
-        #fields = ("ID", "Name", "DisplayName")
