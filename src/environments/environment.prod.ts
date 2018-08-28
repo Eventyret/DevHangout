6a551a4befe999843181fba8e5711851819edc6f
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-28 16:24:16 +0100

    updated production enviroments with test data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-	production: true
+	production: true,
+	publish_api_key: "pk_test_JtDTej8bZw3TXlC6Yly93PSe",
+	github_client_id: "409ff64c5f5012548dcc",
+	github_client_secret: "d5e1898c580216a2dbd6240da0d59f75421cfdc9"
 };
