8d0f3beb96adc32f8b19a0c7caea97c93a7c684e
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-30 22:22:59 +0100

    Form is now reactive, still need to post to backend

--- src/app/dashboard-page/components/experience/edit-experience/edit-experience.component.ts
+++ src/app/dashboard-page/components/experience/edit-experience/edit-experience.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { DataService } from "../../../services/data.service";
+import { Experience } from "../../../../shared/models/users";
 
 @Component({
 	selector: "app-edit-experience",
@@ -7,10 +10,50 @@ import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 	styleUrls: ["./edit-experience.component.scss"]
 })
 export class EditExperienceComponent implements OnInit {
-	name;
-	id;
-	constructor(public activeModal: NgbActiveModal) {}
+	name: string;
+	experience: Experience
+	id: number;
+	current: boolean
+
+	editForm = new FormGroup({
+		company: new FormControl("", Validators.required),
+		jobtitle: new FormControl("", Validators.required),
+		location: new FormControl(""),
+		from: new FormControl("", Validators.required),
+		to: new FormControl(""),
+		current: new FormControl(Validators.required),
+	});
+	constructor(public activeModal: NgbActiveModal, private dataService: DataService) {}
 
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.dataService.getDetailed("experience", this.id).subscribe(
+			(data: Experience) => {
+				this.experience = data;
+				this.editForm.patchValue({
+					company: data.company,
+					jobtitle: data.jobTitle,
+					location: data.location,
+					from: data.dateFrom,
+					to: data.dateTo,
+					current: data.current
+				})
+			}, error => {
+				console.log(error)
+			},
+			() => {
+				this.onChanges();
+			}
+		)
+	}
+
+	onChanges() {
+		this.editForm.get("current").valueChanges.subscribe(val => {
+			this.current = !this.current;
+		})
+	}
+
+	update(form){
+		console.log(form);
+	}
 }
