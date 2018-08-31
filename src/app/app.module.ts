e72cf8a6bbe495b8f4f86c19abde142c24aafe7d
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-31 16:41:57 +0100

    added .forRoot() this was still required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -94,7 +94,7 @@ const routes: Routes = [
 		BrowserModule,
 		BrowserAnimationsModule,
 		RouterModule.forRoot(routes),
-		NgbModule,
+		NgbModule.forRoot(),
 		CommonModule,
 		NgxSpinnerModule,
 		HttpClientModule,
