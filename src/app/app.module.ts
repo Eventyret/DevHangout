947d71886eafc388952c3e74c15a1f343bcf23b8
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-31 11:24:45 +0100

    removed deprivated forRoot in a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -94,7 +94,7 @@ const routes: Routes = [
 		BrowserModule,
 		BrowserAnimationsModule,
 		RouterModule.forRoot(routes),
-		NgbModule.forRoot(),
+		NgbModule,
 		CommonModule,
 		NgxSpinnerModule,
 		HttpClientModule,
