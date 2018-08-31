9c2e5151ce686b8569acfb2a59b1940c4cb86d84
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-31 11:13:04 +0100

    Added markup for adding Experience (No backend functionality yet)

--- src/app/dashboard-page/components/experience/add-experience/add-experience.component.ts
+++ src/app/dashboard-page/components/experience/add-experience/add-experience.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { DataService } from "../../../services/data.service";
+import { Experience } from "../../../../shared/models/users";
+import { Validators, FormGroup, FormControl } from "@angular/forms";
 
 @Component({
 	selector: "app-add-experience",
@@ -7,9 +10,33 @@ import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 	styleUrls: ["./add-experience.component.scss"]
 })
 export class AddExperienceComponent implements OnInit {
-	name;
+	name: string;
+	experience: Experience;
+	id: number;
+	current = false;
 
-	constructor(public activeModal: NgbActiveModal) {}
+	addForm = new FormGroup({
+		company: new FormControl("", Validators.required),
+		jobtitle: new FormControl("", Validators.required),
+		location: new FormControl(""),
+		from: new FormControl("", Validators.required),
+		to: new FormControl(""),
+		current: new FormControl(Validators.required)
+	});
 
-	ngOnInit() {}
+	constructor(public activeModal: NgbActiveModal, private dataService: DataService) {}
+
+	ngOnInit() {
+		this.onChanges();
+	}
+
+	onChanges() {
+		this.addForm.get("current").valueChanges.subscribe(val => {
+			this.current = !this.current;
+		});
+	}
+
+	add(form) {
+		console.log(form);
+	}
 }
