5fb1ea36412e42be096529656a756917fa32d891
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-31 11:24:51 +0100

    fixed linting errors

--- src/app/dashboard-page/services/data.service.spec.ts
+++ src/app/dashboard-page/services/data.service.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
 
-import { DataService } from './data.service';
+import { DataService } from "./data.service";
 
-describe('DataService', () => {
+describe("DataService", () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [DataService]
-    });
+	TestBed.configureTestingModule({
+		providers: [DataService]
+	});
   });
 
-  it('should be created', inject([DataService], (service: DataService) => {
-    expect(service).toBeTruthy();
+  it("should be created", inject([DataService], (service: DataService) => {
+	expect(service).toBeTruthy();
   }));
 });
