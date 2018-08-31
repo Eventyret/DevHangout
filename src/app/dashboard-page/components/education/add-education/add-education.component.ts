f6875ab27d258f5bbaf73065009dc2c50f423305
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-31 11:17:32 +0100

    Made form reactive, still no backend to this component

--- src/app/dashboard-page/components/education/add-education/add-education.component.ts
+++ src/app/dashboard-page/components/education/add-education/add-education.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Education } from "../../../../shared/models/users";
+import { DataService } from "../../../services/data.service";
 
 @Component({
 	selector: "app-add-education",
@@ -7,8 +10,31 @@ import { NgbModal, NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 	styleUrls: ["./add-education.component.scss"]
 })
 export class AddEducationComponent implements OnInit {
-	name;
-	constructor(public activeModal: NgbActiveModal) {}
+	name: string;
+	id: number;
+	education: Education;
+	current: boolean;
 
-	ngOnInit() {}
+	addForm = new FormGroup({
+		school: new FormControl("", Validators.required),
+		degree: new FormControl("", Validators.required),
+		fieldofstudy: new FormControl("", Validators.required),
+		from: new FormControl("", Validators.required),
+		to: new FormControl(),
+		current: new FormControl(Validators.required)
+	});
+
+	constructor(public activeModal: NgbActiveModal, private dataService: DataService) {}
+
+	ngOnInit() {
+		this.onChanges();
+	}
+	onChanges() {
+		this.addForm.get("current").valueChanges.subscribe(val => {
+			this.current = !this.current;
+		});
+	}
+	add(form) {
+		console.log(form);
+	}
 }
