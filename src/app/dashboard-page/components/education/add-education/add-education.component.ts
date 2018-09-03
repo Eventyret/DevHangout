3698d436aacb3fc4831e7cee092464cab0741696
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-03 08:38:08 +0100

    Ability to add education now implemented

--- src/app/dashboard-page/components/education/add-education/add-education.component.ts
+++ src/app/dashboard-page/components/education/add-education/add-education.component.ts
@@ -11,18 +11,21 @@ import { NotificationsService } from "angular2-notifications";
 	styleUrls: ["./add-education.component.scss"]
 })
 export class AddEducationComponent implements OnInit {
+	education: Education;
+	updatedForm: Education;
+	current: boolean;
+	user: number;
 	name: string;
 	id: number;
-	education: Education;
-	current: boolean;
 
 	addForm = new FormGroup({
+		current: new FormControl(false, Validators.required),
+		dateFrom: new FormControl("", Validators.required),
+		dateTo: new FormControl(),
+		fieldOfStudy: new FormControl("", Validators.required),
+		qualification: new FormControl("", Validators.required),
 		school: new FormControl("", Validators.required),
-		degree: new FormControl("", Validators.required),
-		fieldofstudy: new FormControl("", Validators.required),
-		from: new FormControl("", Validators.required),
-		to: new FormControl(),
-		current: new FormControl(Validators.required)
+		user: new FormControl((this.user = parseInt(localStorage.getItem("user_id"), 10)), Validators.required)
 	});
 
 	constructor(public activeModal: NgbActiveModal, private dataService: DataService, private notify: NotificationsService) {}
@@ -32,30 +35,22 @@ export class AddEducationComponent implements OnInit {
 	}
 	onChanges() {
 		this.addForm.valueChanges.subscribe(val => {
-			this.current = !val.current;
+			this.updatedForm = val;
+		});
+		this.addForm.get("current").valueChanges.subscribe(val => {
+			this.current = !this.current;
 		});
 	}
 
-	add(form) {
-		let data = {
-			user: this.id,
-			jobTitle: form.jobTitle,
-			company: form.company,
-			location: form.location,
-			dateFrom: form.dateFrom,
-			dateTo: form.dateTo,
-			current: form.current
-		};
-		this.dataService.newDetails("education", this.id, data).subscribe(
-			results => {
-				console.log(results);
-			},
+	add() {
+		this.dataService.newDetails("education", this.updatedForm).subscribe(
+			results => {},
 			error => {
 				console.log(error);
 				this.notify.error("Seems there was an issue ?", error);
 			},
 			() => {
-				this.notify.success("Experience Added");
+				this.notify.success("Education Added");
 				this.activeModal.close();
 			}
 		);
