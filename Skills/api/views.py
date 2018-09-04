efa65631d2d18577417947e9a0229e2ad775bc9b
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-04 12:21:19 +0100

    Updated Skills per person

--- Skills/api/views.py
+++ Skills/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.contrib.auth.models import User
 from ..models import Skill
 from .serializers import SkillsSerializer
 
@@ -10,3 +11,11 @@ class SkillsView(viewsets.ModelViewSet):
     """ Settings it to read only as its not needed to be updated or deleted from the frontend """
     queryset = Skill.objects.all()
     serializer_class = SkillsSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs["id"]
+        skill_id = self.kwargs.get("pk")
+        if skill_id:
+            return User.objects.get(id=user_id).skills.filter(id=skill_id)
+        else:
+            return User.objects.get(id=user_id).skills
--- Skills/models.py
+++ Skills/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Skill(models.Model):
-	"""List of skills that a developer can pick from"""
-	user = models.ManyToManyField(User, related_name="skills", blank=True)
-	name = models.CharField(max_length=50, blank=True)
-	icon = models.CharField(max_length=100, blank=True)
+    """List of skills that a developer can pick from"""
+    user = models.ManyToManyField(User, related_name="skills", blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    icon = models.CharField(max_length=100, blank=True)
+    owned = models.BooleanField(default=False)
 
-	class Meta:
-		verbose_name_plural = "Skills"
+    class Meta:
+        verbose_name_plural = "Skills"
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
