49826ec7560cb8989a0633f2a011f5c5228a4e6a
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-08 10:34:14 +0100

    Removed old migrations, as we made significant changes to DB

--- BackendApi/Skills/models.py
+++ BackendApi/Skills/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Skill(models.Model):
     """List of skills that a developer can pick from"""
-    user = models.ManyToManyField(User, related_name="skills", blank=True)
+    user = models.ForeignKey(User, related_name="skills", on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=True)
     icon = models.CharField(max_length=100, blank=True)
     owned = models.BooleanField(default=False)
