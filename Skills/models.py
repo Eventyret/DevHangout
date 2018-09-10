331e9db5083f57be433db5765d1d8e20966989d4
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-10 22:43:58 +0100

    Made skills manytomany field

--- Skills/models.py
+++ Skills/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Skill(models.Model):
     """List of skills that a developer can pick from"""
-    user = models.ForeignKey(User, related_name="skills", on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, related_name="skill_id")
     name = models.CharField(max_length=50, blank=True)
     icon = models.CharField(max_length=100, blank=True)
     owned = models.BooleanField(default=False)
