84a0b9cc670180460ed441b2e5296ad9c1c58fae
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-11 17:46:43 +0100

    Re-wrote where we check if the user is logged in or if they have a username

--- src/app/shared/components/fab/fab.component.ts
+++ src/app/shared/components/fab/fab.component.ts
@@ -10,22 +10,26 @@ import { NgbModal, NgbActiveModal, NgbTooltip } from "@ng-bootstrap/ng-bootstrap
 })
 export class FabComponent implements OnInit {
 	comp: any;
-	username: string;
+	name: string;
+	loggedIn: boolean;
 	spin: any;
 	constructor(private modalService: NgbModal) {}
-	ngOnInit() {}
+	ngOnInit() {
+		this.name = "Anonymous";
+	}
 
 	open(event: any) {
 		const target = event.target.id;
-		if (localStorage.getItem("username")) {
-			this.username = localStorage.getItem("username");
-		} else {
-			this.username = "Anonymous";
-		}
 		if (target === "infoButton") {
 			this.comp = InfoModalComponent;
 		} else if (target === "supporterButton") {
 			this.comp = SupporterModalComponent;
+		}
+		if (!localStorage.getItem("username")) {
+			this.loggedIn = false;
+		} else {
+			this.loggedIn = true;
+			this.name = localStorage.getItem("username");
 		}
 		const modalRef = this.modalService.open(this.comp, {
 			centered: true,
@@ -33,6 +37,7 @@ export class FabComponent implements OnInit {
 			backdropClass: "light-blue-backdrop",
 			backdrop: "static"
 		});
-		modalRef.componentInstance.name = this.username;
+		modalRef.componentInstance.name = this.name;
+		modalRef.componentInstance.loggedIn = this.loggedIn;
 	}
 }
