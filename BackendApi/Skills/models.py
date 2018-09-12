c3311e17854d282faf6db9cfbc5bb33641122add
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-12 10:16:10 +0100

    updated skills to have a skillID for filtering if a user has it or not in the frontend

--- BackendApi/Skills/models.py
+++ BackendApi/Skills/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Skill(models.Model):
     """List of skills that a developer can pick from"""
     user = models.ForeignKey(User, related_name="skill", on_delete=models.CASCADE)
+    skillID = models.IntegerField(blank=True, null=True)
     name = models.CharField(max_length=50, blank=True)
     icon = models.CharField(max_length=100, blank=True)
     owned = models.BooleanField(default=False)
