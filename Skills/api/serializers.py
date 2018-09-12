d4fb99f661da390eadc6882694c728b4c12d583d
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-12 11:12:42 +0100

    removed wrong structure

--- Skills/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ..models import Skill
-
-
-class SkillsSerializer(serializers.ModelSerializer):
-    """ A Serializer that will have read only access but using the modal one """
-    class Meta:
-        model = Skill()
-        fields = ("user", "name", "icon", "owned")
