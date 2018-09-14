512daab834004e0265336808fc7f630c06c521a3
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-15 00:57:08 +0100

    Commented and removed unused code

--- src/app/shared/components/session-expired/session-expired.component.ts
+++ src/app/shared/components/session-expired/session-expired.component.ts
@@ -4,48 +4,70 @@ import { AuthService } from "../../services/auth/auth.service";
 import { Router } from "@angular/router";
 import { NgbActiveModal, NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { SignupComponent } from "../signup/signup.component";
+import { User } from "../../models/users.model";
 @Component({
 	selector: "app-session-expired",
 	templateUrl: "./session-expired.component.html",
 	styleUrls: ["./session-expired.component.scss"]
 })
 export class SessionExpiredComponent implements OnInit {
-	invalidLogin: boolean;
-	username: string;
-	newUser: boolean;
+	public invalidUser: boolean;
+	public username: string;
 	loginForm = new FormGroup({
 		username: new FormControl("", Validators.required),
 		password: new FormControl("", Validators.required)
 	});
 
-	get usernameField() {
-		return this.loginForm.get("username");
-	}
-	constructor(private router: Router, private authService: AuthService, public activeModal: NgbActiveModal, private modalService: NgbModal) {}
+	constructor(
+		private router: Router,
+		private authService: AuthService,
+		public activeModal: NgbActiveModal,
+		private modalService: NgbModal
+		) {}
+
 
 	ngOnInit() {
-		this.username = localStorage.getItem("username");
-		if (!localStorage.getItem("username")) {
-			this.newUser = true;
-		} else {
-			this.newUser = false;
-		}
 	}
 
-	signIn(credentials) {
+
+	/**
+	 * Signs the user in.
+	 *  If the login is successfull we will redirect the user
+	 * to the front page and dismiss the modal
+	 * @param credentials
+	 * @fires Router Navigating the user to front page
+	 */
+	signIn(credentials: User) {
 		this.authService.login(credentials).subscribe(results => {
 			if (results) {
 				this.router.navigate(["/"]);
 				this.activeModal.dismiss();
 			} else {
-				this.invalidLogin = true;
+				this.invalidUser = true;
 			}
 		});
 	}
+
+
+	/**
+	 * If the user closes the modal
+	 * we will redirect the user to the front page
+	 * and notify the user
+	 *
+	 */
 	dismiss() {
 		this.authService.sessionExpired();
 		this.activeModal.dismiss();
 	}
+
+
+
+	/**
+	 * If the user wants to register or there
+	 * has been an unknown error.
+	 * We will open the SignupComponent and let the user register
+	 *
+	 */
 	register() {
 		this.activeModal.dismiss();
 		this.modalService.open(SignupComponent, {
@@ -53,5 +75,13 @@ export class SessionExpiredComponent implements OnInit {
 			size: "lg",
 			backdrop: "static"
 		});
+	}
+
+
+	/**
+	 * Gets username field
+	 */
+	get usernameField() {
+		return this.loginForm.get("username");
 	}
 }
