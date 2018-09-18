c7fb6ff1aec81d73d4392323c0019a7e9fc29d41
Simen Daehlin <simen.dehlin@gmail.com>
2018-09-18 18:35:21 +0100

    Fixed a bug with testimonals not linking to the profile.

--- src/app/home-page/components/testimonials/testimonials.component.ts
+++ src/app/home-page/components/testimonials/testimonials.component.ts
@@ -3,32 +3,31 @@ import { sampleSize as _sampleSize } from "lodash";
 import { FakeService } from "../../../shared/services/api/fake.service";
 
 @Component({
-  selector: "app-testimonials",
-  templateUrl: "./testimonials.component.html",
-  styleUrls: ["./testimonials.component.scss"]
+	selector: "app-testimonials",
+	templateUrl: "./testimonials.component.html",
+	styleUrls: ["./testimonials.component.scss"]
 })
 export class TestimonialsComponent implements OnInit {
 	public testimonials: any[];
 
+	/**
+	 * Creates an instance of testimonials component.
+	 * @param fakeService This is the service getting the fake users.
+	 */
+	constructor(private fakeService: FakeService) {}
 
-  /**
-   * Creates an instance of testimonials component.
-   * @param fakeService This is the service getting the fake users.
-   */
-  constructor(private fakeService: FakeService) { }
+	ngOnInit() {
+		this.getTestimonials();
+	}
 
-  ngOnInit() {
-	this.getTestimonials();
-}
-
-
-/**
- * Gets testimonials from the fake users.
- * Then we generate a new list of 20 on random and display 6 of them
- */
-private getTestimonials() {
-	this.fakeService.getFakeUsers().subscribe(data => {
-		this.testimonials = _sampleSize(data, 20);
-	});
-}
+	/**
+	 * Gets testimonials from the fake users.
+	 * Then we generate a new list of 20 on random and display 6 of them
+	 */
+	private getTestimonials() {
+		this.fakeService.getFakeUsers().subscribe(data => {
+			this.testimonials = _sampleSize(data, 20);
+			console.log(this.testimonials);
+		});
+	}
 }
